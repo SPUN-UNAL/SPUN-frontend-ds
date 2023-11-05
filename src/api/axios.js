6ba74369572f71561a1c977c8753c12399edeb1f
Daniel Diaz <danieldiazgonza52@gmail.com>
2023-11-05 15:34:26 -0500

    Fixed axios issues

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import { BACKEND_URL } from "./config";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: BACKEND_URL,
   withCredentials: true,
 });
 
--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,1 @@
-import dotenv from 'dotenv';
-dotenv.config();
-export const BACKEND_URL = process.env.BACKEND_URL;
+export const BACKEND_URL = "http://localhost:4000/api";

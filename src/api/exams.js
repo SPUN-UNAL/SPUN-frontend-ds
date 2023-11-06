3445a999733d7730286223eecaae13a9dae79daa
Daniel Diaz <danieldiazgonza52@gmail.com>
2023-11-06 17:18:49 -0500

    Corrected code style api

--- src/api/exams.js
+++ src/api/exams.js
@@ -1,12 +1,12 @@
 import axios from "./axios";
 
-export const getExam = async  (id) => {
-    return axios.get(`/exam/${id}`);
+export const getExam = async (id) => {
+  return axios.get(`/exam/${id}`);
 };
 
-export const getExams = async ()=>{
+export const getExams = async () => {
   return axios.get(`/exam/`);
-}
+};
 
 export const postExamTake = async (
   exam_id,
@@ -22,7 +22,7 @@ export const postExamTake = async (
     endExam: endExam,
     email: email,
     score: score,
-  }
+  };
   try {
     axios.post(`/exam/take/${exam_id}`, data);
   } catch (error) {
